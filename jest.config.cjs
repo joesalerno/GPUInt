7d22d076a4211c59262a5f4d6866edc94adb7d29
Joe Salerno <joesalerno.js@gmail.com>
2025-06-06 16:04:50 -0400

    Cleaning up vite install

--- jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: "jest-puppeteer",
-    testMatch: ["**/__tests__/**/*.+(browser.test).[jt]s?(x)", "**/?(*.)+(browser.test).[jt]s?(x)"],
-    transform: {
-        '^.+\\.m?js$': 'babel-jest' // Process .js and .mjs files with babel-jest
-    }
-};
